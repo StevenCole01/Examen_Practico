1aa8521caaab12fcfd237705bb4a1ccf0f9c33cb
StevenCole01 <56403389+StevenCole01@users.noreply.github.com>
2022-05-04 23:42:11 -0700

    Update receptor.py

--- receptor.py
+++ receptor.py
@@ -1,7 +1,19 @@
-import time
-from socket import socket,AF_INET, SOCK_DGRAM
+from socket import socket,AF_INET, SOCK_DGRAM #Importar modulos para la comunicacion de sockets
+import serial, time #Importar modulo para comunicacion con puerto serial, y manejo de tiempo
 
-s = socket(AF_INET, SOCK_DGRAM)
+#Definicion de las variables necesarias
+PUERTO = "COM7"
+BAUDIOS = 9600
+IP = "189.223.71.122" #IP de Antonio
+#IP = "201.171.13.53" #IP de Velazco
+
+s = socket(AF_INET, SOCK_DGRAM) #Creacion del socket
+arduino = serial.Serial(PUERTO, BAUDIOS) #Creación del objeto serial para la comunicacion con el arduino receptor
+
 while(True):
-    print(s.recv(8192))
-    time.sleep(0.01)
+    s.sendto(b"receptor:2", (IP, 443)) #Se envia confirmacion de conexion al servidor enviando el mensaje
+    x = s.recv(8192) #Se guarda en x lo recibido por el socket conectado al servidor
+    time.sleep(0.5)
+    if(x==b'1' or x==b'0'): 
+        print(x)
+        arduino.write(x) #Se manda x al arduino por el puerto serie
